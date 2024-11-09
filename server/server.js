4bfd2f7c4e6d1a5da82fbadfcfa6c7605ac46c60
manojLovesCoding <mkum9417@gmail.com>
2024-11-09 20:20:22 +0530

    webhook added

--- server/server.js
+++ server/server.js
@@ -2,6 +2,8 @@ import 'dotenv/config'
 import express from 'express'
 import cors from 'cors'
 import connectDB from './configs/mongodb.js'
+import userRouter from './routes/userRoutes.js'
+
 
 
 //App config
@@ -12,6 +14,7 @@ await connectDB()
 //Intialize Middlewares
 app.use(express.json())
 app.use(cors())
+app.use('/api/user', userRouter)
 
 //API Routes
 app.get('/', (req, res) => res.send("API WORKING"))
